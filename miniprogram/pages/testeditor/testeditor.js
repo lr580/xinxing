046ae580b3e01efa51ed52c6287a7bd1f090786f
lr580 <2277379149@qq.com>
2021-04-27 12:46:23 +0800

    调试editor

--- miniprogram/pages/testeditor/testeditor.js
+++ miniprogram/pages/testeditor/testeditor.js
@@ -1,4 +1,4 @@
-// miniprogram/pages/index/index.js
+// miniprogram/pages/testeditor/testeditor.js
 Page({
 
   /**
@@ -15,17 +15,28 @@ Page({
 
   },
 
-  totest:function(){
-    wx.navigateTo({
-      url: '/pages/testeditor/editor',
-    })
-  },  
+  onEditorReady() {
+    const that = this
+    const query = wx.createSelectorQuery()//创建节点查询器
+    query.in(that).select('#editor').context()//选择id=editor的节点，获取节点内容信息
+    
+    query.exec(function(res){
+        that.editorCtx = res.context
+        console.log(res.context);
+      })
+   },
 
-  totest2:function(){
-    wx.navigateTo({
-      url: '/pages/testsummon/testsummon',
+   getContent() {
+    const that = this
+    that.editorCtx.getContents({
+      success: function (res) {   
+        console.log(res.detla)
+      },
+      fail: function (error){
+        console.log(error)
+      }
     })
-  },  
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
---
+++ miniprogram/pages/testsummon/testsummon.js
@@ -0,0 +1,95 @@
+var WxParse = require('../../wxParse/wxParse.js');
+import api from '../../utils/api.js';
+Page({
+  data: {
+		try:'<h1>123</h1><h2>34</h2>',
+  },
+  onLoad: function () {
+    var that = this;
+    /**
+     * 初始化emoji设置
+     */
+    WxParse.emojisInit('[]', "../../wxParse/emojis/", {
+      "00": "00.gif",
+      "01": "01.gif",
+      "02": "02.gif",
+      "03": "03.gif",
+      "04": "04.gif",
+      "05": "05.gif",
+      "06": "06.gif",
+      "07": "07.gif",
+      "08": "08.gif",
+      "09": "09.gif",
+      "09": "09.gif",
+      "10": "10.gif",
+      "11": "11.gif",
+      "12": "12.gif",
+      "13": "13.gif",
+      "14": "14.gif",
+      "15": "15.gif",
+      "16": "16.gif",
+      "17": "17.gif",
+      "18": "18.gif",
+      "19": "19.gif",
+    });
+    /**
+     * html解析示例
+     */
+    var article='<h1>123</h1><h2>34</h2>'
+		
+    
+    WxParse.wxParse('article', 'html', article, that, 5);
+
+
+    /**
+     * 多数据解析示例
+     */
+    var replyHtml0 = `<div style="margin-top:10px;height:50px;">
+		<p class="reply">
+			wxParse回复0:不错，喜欢[03][04]
+		</p>	
+	</div>`;
+    var replyHtml1 = `<div style="margin-top:10px;height:50px;">
+		<p class="reply">
+			wxParse回复1:不错，喜欢[03][04]
+		</p>	
+	</div>`;
+    var replyHtml2 = `<div style="margin-top:10px;height:50px;">
+		<p class="reply">
+			wxParse回复2:不错，喜欢[05][07]
+		</p>	
+	</div>`;
+    var replyHtml3 = `<div style="margin-top:10px;height:50px;">
+		<p class="reply">
+			wxParse回复3:不错，喜欢[06][08]
+		</p>	
+	</div>`;
+    var replyHtml4 = `<div style="margin-top:10px; height:50px;">
+		<p class="reply">
+			wxParse回复4:不错，喜欢[09][08]
+		</p>	
+	</div>`;
+    var replyHtml5 = `<div style="margin-top:10px;height:50px;">
+		<p class="reply">
+			wxParse回复5:不错，喜欢[07][08]
+		</p>	
+	</div>`;
+    var replyArr = [];
+    replyArr.push(replyHtml0);
+    replyArr.push(replyHtml1);
+    replyArr.push(replyHtml2);
+    replyArr.push(replyHtml3);
+    replyArr.push(replyHtml4);
+    replyArr.push(replyHtml5);
+
+
+    for (let i = 0; i < replyArr.length; i++) {
+      WxParse.wxParse('reply' + i, 'html', replyArr[i], that);
+      if (i === replyArr.length - 1) {
+        WxParse.wxParseTemArray("replyTemArray",'reply', replyArr.length, that)
+      }
+    }
+  }
+
+
+})
